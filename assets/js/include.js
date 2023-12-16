f9e515aefd0c531bc9b143c6c6968806852f1688
KCY <cks402@naver.com>
2023-12-16 20:07:56 +0900

    ♻️ xhttp 로딩을 fetch로딩으로 전환
    우리가 보통 xhttp는 접한적이 없어도 fetch는 접한적이 있으니까 더 친숙하리라 생각하기 떄문에 fetch로 변환하여 작성햇습니다.

--- assets/js/include.js
+++ assets/js/include.js
@@ -3,34 +3,26 @@ function includeHtml() {
     includeTarget.forEach(function (el, idx) {
         const targetFile = el.dataset.includeFile;
         if (targetFile) {
-            let xhttp = new XMLHttpRequest();
-
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === XMLHttpRequest.DONE) {
-                    if (this.status === 200) {
-                        const parser = new DOMParser();
-                        const htmlDoc = parser.parseFromString(
-                            this.responseText,
-                            "text/html"
-                        );
-                        const currentHref = window.location.pathname;
-                        console.log(currentHref);
-                        const activeElement = htmlDoc.querySelector(
-                            `[href="${currentHref}"]`
-                        );
-                        if (activeElement) {
-                            activeElement.classList.add("active");
-                        }
-                        el.innerHTML = htmlDoc.body.innerHTML;
-                        logout();
-                    } else if (this.status === 404) {
-                        el.innerHTML = "include not found.";
+            fetch(targetFile)
+                .then((response) => response.text())
+                .then((html) => {
+                    const parser = new DOMParser();
+                    const htmlDoc = parser.parseFromString(html, "text/html");
+                    const currentHref = window.location.pathname;
+                    console.log(currentHref);
+                    const activeElement = htmlDoc.querySelector(
+                        `[href="${currentHref}"]`
+                    );
+                    if (activeElement) {
+                        activeElement.classList.add("active");
                     }
-                }
-            };
-            xhttp.open("GET", targetFile, true);
-            xhttp.send();
-            return;
+                    el.innerHTML = htmlDoc.body.innerHTML;
+                    logout();
+                })
+                .catch((error) => {
+                    console.error("Fetch error:", error);
+                    el.innerHTML = "include not found.";
+                });
         }
     });
 }
