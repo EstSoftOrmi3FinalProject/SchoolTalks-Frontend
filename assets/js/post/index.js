5c66af5c30d47ba190f2e8fe68d4679a0665053f
KCY <cks402@naver.com>
2023-12-19 23:27:09 +0900

    ✨ 검색 및 페이지네이션

--- assets/js/post/index.js
+++ assets/js/post/index.js
@@ -1,7 +1,10 @@
 const btnWrite = document.querySelector("#btn-write");
+const btnSearch = document.querySelector("#btn-search");
+const postlist = document.querySelector(".postlist");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
 
-function postload() {
-    const postlist = document.querySelector(".postlist");
+function fetchPost() {
     document.addEventListener("DOMContentLoaded", function () {
         fetch("http://127.0.0.1:8000/post/", {
             method: "GET",
@@ -10,25 +13,7 @@ function postload() {
             },
         })
             .then((res) => res.json())
-            .then((data) => {
-                data.forEach((post) => {
-                    const dateObject = new Date(post.created_at);
-                    const formattedDate = `
-                    ${dateObject.getFullYear()} -
-                    ${dateObject.getMonth() + 1} -
-                    ${dateObject.getDate()}
-                    `;
-                    postlist.innerHTML += `
-                    <tr>
-                        <td class="text-center">${post.id}</td>
-                        <td><a href="/post/post.html?post_id=${post.id}">${post.title}</a></td>
-                        <td class="text-center">${post.author_name}</td>
-                        <td class="text-center">${formattedDate}</td>
-                        <td class="text-center">${post.hits}</td>
-                    </tr>
-                    `;
-                });
-            });
+            .then((data) => postload(data));
     });
 }
 
@@ -39,9 +24,95 @@ function writeload() {
     }
 }
 
-postload();
+function formatDate(isoDate) {
+    const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+    };
+    return new Date(isoDate).toLocaleString("ko-KR", options);
+}
+
+function postload(data) {
+    postlist.innerHTML = `
+    <tr>
+        <th class="col-1 text-center">글번호</th>
+        <th class="col text-center">글제목</th>
+        <th class="col-2 text-center">작성자</th>
+        <th class="col-2 text-center">작성일자</th>
+        <th class="col-1 text-center">조회수</th>
+    </tr>
+    `;
+    data.results.forEach((post) => {
+        const date = formatDate(post.created_at);
+        postlist.innerHTML += `
+        <tr>
+            <td class="text-center">${post.id}</td>
+            <td><a href="/post/post.html?post_id=${post.id}">${post.title}</a></td>
+            <td class="text-center">${post.author_name}</td>
+            <td class="text-center">${date}</td>
+            <td class="text-center">${post.hits}</td>
+        </tr>
+        `;
+    });
+    if (data.previous) {
+        previous.classList.remove("d-none");
+        previous.dataset.url = data.previous;
+    } else {
+        previous.classList.add("d-none");
+    }
+    if (data.next) {
+        next.classList.remove("d-none");
+        next.dataset.url = data.next;
+    } else {
+        next.classList.add("d-none");
+    }
+}
+
+fetchPost();
 writeload();
 
 btnWrite.addEventListener("click", function () {
     window.location.href = "/post/write.html";
+});
+
+// 검색 기능
+
+btnSearch.addEventListener("click", function (e) {
+    e.preventDefault();
+    const type = document.querySelector("#search-select");
+    const search = document.querySelector("#search-value");
+    fetch(`http://127.0.0.1:8000/post/?${type.value}=${search.value}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => postload(data));
+});
+
+// 페이지네이션 기능
+previous.addEventListener("click", function () {
+    fetch(previous.dataset.url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => postload(data));
+});
+
+next.addEventListener("click", function () {
+    fetch(next.dataset.url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => postload(data));
 });
