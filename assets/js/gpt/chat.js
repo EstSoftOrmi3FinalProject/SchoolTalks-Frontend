9bf3ad9f1e689a6e4d72ecf2923852fe9c1a7b89
KimDongHu <ehdgn1022@naver.com>
2023-12-23 18:20:02 +0900

    ♻️ Ai Chat 채팅 말풍선 수정

--- assets/js/gpt/chat.js
+++ assets/js/gpt/chat.js
@@ -1,77 +1,160 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.setTimeout(fadeout, 500);
+document.addEventListener('DOMContentLoaded', async function () {
+    // 일정 시간이 지난 후 프리로더 숨기기
+    window.setTimeout(() => {
+        fadeout(".preloader");
+    }, 500);
 
-    function fadeout() {
-        document.querySelector(".preloader").style.opacity = "0";
-        document.querySelector(".preloader").style.display = "none";
-    }
-
-    // 페이지 로드 시 이전 채팅을 가져와 화면에 표시
-    loadPreviousMessages();
+    // 페이지 로드 시 이전 메시지 로드
+    await loadPreviousMessages();
 });
+
+function fadeout(selector) {
+    const preloader = document.querySelector(selector);
+    preloader.style.opacity = "0";
+    preloader.style.display = "none";
+}
+
+function scrollToBottom() {
+    var chatContainer = document.querySelector('.messages');
+    chatContainer.scroll({
+        top: chatContainer.scrollHeight,
+        behavior: 'smooth'
+    });
+}
 
 async function loadPreviousMessages() {
     try {
-        // 로컬 스토리지에서 토큰 가져오기
         const token = localStorage.getItem('access_token');
+        const apiUrl = 'http://127.0.0.1:8000/aichat/';
 
-        // GET 요청을 통해 이전 채팅 메시지 가져오기
-        const previousMessages = await apiGet('http://127.0.0.1:8000/aichat/', {
+        // 이전 메시지를 가져오기 위한 GET 요청
+        const previousMessages = await apiGet(apiUrl, {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json',
         });
 
-        // 화면에 이전 채팅 메시지 표시
-        var chatContainer = document.getElementById('chat-container');
-        chatContainer.innerHTML = ''; // 이전 채팅 삭제
+        // 이전 채팅 영역 초기화
+        var chatContainer = document.querySelector('.messages');
+        chatContainer.innerHTML = '';
+
+        // 이전 메시지 추가
         previousMessages.forEach(message => {
-            chatContainer.innerHTML += `<p>User: ${message.prompt}</p>`;
-            chatContainer.innerHTML += `<p>AI: ${message.response}</p>`;
+            // 오른쪽에는 User의 질문
+            chatContainer.innerHTML += `<li class="message right">
+                <div class="avatar"></div>
+                <div class="text_wrapper">
+                    <div class="text">${message.prompt}</div>
+                </div>
+            </li>`;
+            
+            // 왼쪽에는 AI의 답변
+            chatContainer.innerHTML += `<li class="message left">
+                <div class="avatar"></div>
+                <div class="text_wrapper">
+                    <div class="text">${message.response}</div>
+                </div>
+            </li>`;
         });
+
+        scrollToBottom();
+
     } catch (error) {
         console.error(error);
-        alert('Failed to load previous messages. Please try again.');
+        alert('이전 메시지를 불러오는 데 실패했습니다. 다시 시도해주세요.');
     }
 }
 
 async function sendMessage() {
     try {
-        // 로컬 스토리지에서 토큰 가져오기
         const token = localStorage.getItem('access_token');
+        const apiUrl = 'http://127.0.0.1:8000/aichat/';
 
         // 입력된 채팅 가져오기
         const userMessage = document.getElementById('chat-input').value;
 
+        // 텍스트 필드 초기화
+        document.getElementById('chat-input').value = '';
+
+        // 사용자의 채팅 화면에 표시
+        appendUserMessage(userMessage);
+
+        // AI의 답변을 기다리면서 로딩 상태 표시
+        appendLoadingMessage();
+
+        scrollToBottom();
+
         // POST 요청으로 새로운 채팅 추가
-        await apiPost('http://127.0.0.1:8000/aichat/', {
+        const aiResponse = await apiPost(apiUrl, {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json',
         }, { prompt: userMessage });
 
-        // 입력창 비우고 이전 채팅 다시 로드
-        document.getElementById('chat-input').value = '';
-        loadPreviousMessages();
+        // 로딩 중 메시지 제거
+        removeLoadingMessage();
+
+        // AI의 답변 화면에 표시
+        appendAiMessage(aiResponse.response);
     } catch (error) {
         console.error(error);
-        alert('Failed to send message. Please try again.');
+        alert('메시지를 전송하는 데 실패했습니다. 다시 시도해주세요.');
     }
+}
+
+function appendUserMessage(message) {
+    var chatContainer = document.querySelector('.messages');
+    // 오른쪽에는 User의 질문
+    chatContainer.innerHTML += `<li class="message right">
+        <div class="avatar"></div>
+        <div class="text_wrapper">
+            <div class="text">${message}</div>
+        </div>
+    </li>`;
+}
+
+function appendLoadingMessage() {
+    var chatContainer = document.querySelector('.messages');
+    // 로딩 중 메시지 표시
+    chatContainer.innerHTML += `<li class="message left" id="loading-message">
+        <div class="avatar"></div>
+        <div class="text_wrapper">
+            <div class="text">...</div>
+        </div>
+    </li>`;
+}
+
+function removeLoadingMessage() {
+    var loadingMessage = document.getElementById('loading-message');
+    if (loadingMessage) {
+        loadingMessage.remove();
+    }
+}
+
+function appendAiMessage(response) {
+    var chatContainer = document.querySelector('.messages');
+    // 왼쪽에는 AI의 답변
+    chatContainer.innerHTML += `<li class="message left">
+        <div class="avatar"></div>
+        <div class="text_wrapper">
+            <div class="text">${response}</div>
+        </div>
+    </li>`;
 }
 
 async function deletePreviousMessages() {
     try {
-        // 로컬 스토리지에서 토큰 가져오기
         const token = localStorage.getItem('access_token');
+        const apiUrl = 'http://127.0.0.1:8000/aichat/';
 
-        // DELETE 요청으로 이전 채팅 삭제
-        await apiDelete('http://127.0.0.1:8000/aichat/', {
+        // DELETE 요청으로 이전 메시지 삭제
+        await apiDelete(apiUrl, {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json',
         });
 
-        // 이전 채팅 삭제 후 화면 갱신
-        loadPreviousMessages();
+        // 이전 메시지 삭제 후 화면 갱신
+        await loadPreviousMessages();
     } catch (error) {
         console.error(error);
-        alert('Failed to delete previous messages. Please try again.');
+        alert('이전 메시지를 삭제하는 데 실패했습니다. 다시 시도해주세요.');
     }
 }
